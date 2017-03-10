d707d8d66bff5433e10ce8b2bebb6ced33bb8595
sonbeams <son_beams@hotmail.com>
2017-03-10 15:30:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => 'เลขบัญชีของท่านคือ'.$text //$text
 			];*/
-			/*$message = [
+			$messages = [
 				'type' => 'template',
 				'altText' => 'this is a confirm template';
 				'template' => [
@@ -40,14 +40,14 @@ if (!is_null($events['events'])) {
 						}
 					]
 				]
-			];*/
-			$messages = [
+			];
+			/*$messages = [
 				'type' => 'location',
     				'title' => 'my location',
     				'address' => '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
     				'latitude' => 35.65910807942215,
     				'longitude' => 139.70372892916203
-			];
+			];*/
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
