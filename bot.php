ce66cf15b76b760b7309988e27265f8d2f7a3d37
sonbeams <son_beams@hotmail.com>
2017-03-10 14:42:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IIckEKH4AEo7xgc74LJmTYBxU39gbny9jEwIbmroCsSTMFmg8RpQ1QPgVIm7kqrR4yO/0g0l/JvCX30uMq+WdFhjDXNuvZfo96+IrLgSZxJ2m2spr+eTIVo17dniDcIknwVf5BvWSFAs0yV3MuGY/gdB04t89/1O/w1cDnyilFU=';
+/*$access_token = 'IIckEKH4AEo7xgc74LJmTYBxU39gbny9jEwIbmroCsSTMFmg8RpQ1QPgVIm7kqrR4yO/0g0l/JvCX30uMq+WdFhjDXNuvZfo96+IrLgSZxJ2m2spr+eTIVo17dniDcIknwVf5BvWSFAs0yV3MuGY/gdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -45,3 +45,63 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+*/
+namespace frontend\controllers;
+
+use frontend\models\LineBot;
+use frontend\modules\forum\models\Thread;
+use yii\helpers\Url;
+
+class LineBotController extends \yii\web\Controller
+{
+
+    public function actionCurl()
+    {
+        /*
+         * ส่งจาก Forum
+         */
+        $last_thread = LineBot::findOne(['type' => 'forum']);
+        $thread = Thread::find()->orderBy(['id' => SORT_DESC])->one();
+        if(!$last_thread){
+            $last_thread = new LineBot();
+            $last_thread->type = 'forum';
+            $last_thread->last_id = $thread->id;
+            $last_thread->save();
+            $message = $thread->subject.' '.Url::to('https://www.programmerthailand.com/forum/view/'.$thread->id);
+            $res = $this->notify_message($message);
+
+        }else{
+            if($last_thread->last_id != $thread->id){
+                $message = $thread->subject.' '.Url::to('https://www.programmerthailand.com/forum/view/'.$thread->id);
+                $res = $this->notify_message($message);
+                $last_thread->last_id = $thread->id;
+                $last_thread->save();
+            }
+        }
+
+
+    }
+
+    public function notify_message($message)
+    {
+        $line_api = 'https://notify-api.line.me/api/notify';
+        $line_token = 'IIckEKH4AEo7xgc74LJmTYBxU39gbny9jEwIbmroCsSTMFmg8RpQ1QPgVIm7kqrR4yO/0g0l/JvCX30uMq+WdFhjDXNuvZfo96+IrLgSZxJ2m2spr+eTIVo17dniDcIknwVf5BvWSFAs0yV3MuGY/gdB04t89/1O/w1cDnyilFU=';
+
+        $queryData = array('message' => $message);
+        $queryData = http_build_query($queryData,'','&');
+        $headerOptions = array(
+            'http'=>array(
+                'method'=>'POST',
+                'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                    ."Authorization: Bearer ".$line_token."\r\n"
+                    ."Content-Length: ".strlen($queryData)."\r\n",
+                'content' => $queryData
+            )
+        );
+        $context = stream_context_create($headerOptions);
+        $result = file_get_contents($line_api, FALSE, $context);
+        $res = json_decode($result);
+        return $res;
+    }
+
+}
