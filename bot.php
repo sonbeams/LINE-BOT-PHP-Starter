a2972f502b35a35a39a09c7c5da318b18e3600bf
sonbeams <son_beams@hotmail.com>
2017-03-14 14:27:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			 Build message to reply back
-			/*$messages = [
+			$messages = [
 				'type' => 'text',
 				'text' => 'เลขบัญชีของท่านคือ'.$text //$text
 			];
