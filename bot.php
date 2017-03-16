ecfa642dd3970ff2ba0927cf213da336505162b1
sonbeams <son_beams@hotmail.com>
2017-03-16 15:24:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -64,7 +64,7 @@ if (!is_null($events['events'])) {
 			if ($text == 'imgtest') {
 				$messages = [
 					'type' => 'imagemap',
-					'baseUrl' => 'https://www.img.in.th/images/18c25e35d2b0c2fb9066f550f20deb06.png',
+					'baseUrl' => 'https://www.img.in.th/images/18c25e35d2b0c2fb9066f550f20deb06.png/1040',
 					'altText' => 'This is an imagemap',
 					'baseSize' => array(
 						"height" => 1040,
@@ -73,7 +73,7 @@ if (!is_null($events['events'])) {
 					'actions' => array(
 						array(
 						    "type" => "uri",
-						    "linkUri" => "https://www.img.in.th/images/18c25e35d2b0c2fb9066f550f20deb06.png/1040",
+						    "linkUri" => "https://www.trinityquicktrade.com",
 						    "area" => array(
 							    "x" => 0,
 							    "y" => 0,
