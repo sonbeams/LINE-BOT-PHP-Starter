df7503e14a67fd3a99d08ba1419fad1e2c0ba389
sonbeams <son_beams@hotmail.com>
2017-04-04 15:53:10 +0700

    Update createimg.php

--- createimg.php
+++ createimg.php
@@ -1,12 +1,13 @@
 <?php
 
 header("Content-type: image/png");
-$string = $_GET['text'];
-$im     = imagecreatefrompng("images/button1.png");
+$string = 'test';
+$im     = imagecreatefrompng("https://img.in.th/images/625ad187dca740ce48ad4688a97d6fef.png");
 $orange = imagecolorallocate($im, 220, 210, 60);
 $px     = (imagesx($im) - 7.5 * strlen($string)) / 2;
 imagestring($im, 3, $px, 9, $string, $orange);
 imagepng($im);
 imagedestroy($im);
 
+echo "OK";
 ?>
